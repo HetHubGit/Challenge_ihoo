0aae1ea067754e8bec6de66453074d9f44298f59
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-28 11:02:48 +0100

    testy

--- User.cs
+++ User.cs
@@ -23,21 +23,40 @@
         }
         public static void Main(string[] args)
         {
+            int maxValue = -1;
+            User maxValueUser = null;
             User user1 = new User("Daniel", "Pass738");
             User user2 = new User("Julia", "Pass738");
+            User user = new User("Jacek", "pass6567");
+
             List<User> listofUsers = new List<User>()
             {
                 user1, user2
-            };
+            };            
+            foreach(var user0 in listofUsers)
+            {
+                if(user0.Result > maxValue)
+                {
+                    maxValueUser = user0;
+                }
+                Console.WriteLine(user0);
+                Console.WriteLine(maxValueUser);
+            }
+            user.AddScore(1);
+            user.AddScore(3);
             user1.AddScore(5);
             user1.AddScore(2);
             user2.AddScore(2);
             user2.AddScore(8);
+            var result0 = user.Result;
+            Console.WriteLine(result0);
             var result1 = user1.Result;
             Console.WriteLine(result1);
             var result2 = user2.Result;
             Console.WriteLine(result2);
-            var allScore = result1 + result2;  
+            var allScore = result0 + result1 + result2;
+            Console.WriteLine(allScore);
+            Console.WriteLine("Uzytkownik; ", user.login);
         }
     }
 }
